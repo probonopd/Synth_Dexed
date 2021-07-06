2c7b00ab3f0e5364535b279e79b39f669a4a80bc
Holger Wirtz <wirtz@dfn.de>
2021-07-06 13:02:37 +0200

    Cleanup.

--- synth_dexed.h
+++ synth_dexed.h
@@ -1,8 +1,97 @@
+/*
+   synth_dexed
+
+   synth_dexed is a port of the Dexed sound engine (https://github.com/asb2m10/dexed)
+   as library for the Teensy-3.5/3.6/4.x with an audio shield.
+   Dexed ist heavily based on https://github.com/google/music-synthesizer-for-android
+
+   (c)2018-2021 H. Wirtz <wirtz@parasitstudio.de>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
+
 #pragma once
 
 #include <Arduino.h>
 #include <Audio.h>
-#include "config.h"
+
+#define SYNTH_DEXED_VERSION "1.0"
+#define DEBUG 1
+#define SAMPLE_RATE 44100
+
+#define MIDI_CONTROLLER_MODE_MAX 2
+#define MAX_NOTES 16
+#define TRANSPOSE_FIX 24
+#define VOICE_SILENCE_LEVEL 1100
+
+#define PB_RANGE_MIN 0
+#define PB_RANGE_MAX 12
+#define PB_RANGE_DEFAULT 1
+
+#define PB_STEP_MIN 0
+#define PB_STEP_MAX 12
+#define PB_STEP_DEFAULT 0
+
+#define MW_RANGE_MIN 0
+#define MW_RANGE_MAX 99
+#define MW_RANGE_DEFAULT 50
+
+#define MW_ASSIGN_MIN 0
+#define MW_ASSIGN_MAX 7
+#define MW_ASSIGN_DEFAULT 0 // Bitmapped: 0: Pitch, 1: Amp, 2: Bias
+
+#define MW_MODE_MIN 0
+#define MW_MODE_MAX MIDI_CONTROLLER_MODE_MAX
+#define MW_MODE_DEFAULT 0
+
+#define FC_RANGE_MIN 0
+#define FC_RANGE_MAX 99
+#define FC_RANGE_DEFAULT 50
+
+#define FC_ASSIGN_MIN 0
+#define FC_ASSIGN_MAX 7
+#define FC_ASSIGN_DEFAULT 0 // Bitmapped: 0: Pitch, 1: Amp, 2: Bias
+
+#define FC_MODE_MIN 0
+#define FC_MODE_MAX MIDI_CONTROLLER_MODE_MAX
+#define FC_MODE_DEFAULT 0
+
+#define BC_RANGE_MIN 0
+#define BC_RANGE_MAX 99
+#define BC_RANGE_DEFAULT 50
+
+#define BC_ASSIGN_MIN 0
+#define BC_ASSIGN_MAX 7
+#define BC_ASSIGN_DEFAULT 0 // Bitmapped: 0: Pitch, 1: Amp, 2: Bias
+
+#define BC_MODE_MIN 0
+#define BC_MODE_MAX MIDI_CONTROLLER_MODE_MAX
+#define BC_MODE_DEFAULT 0
+
+#define AT_RANGE_MIN 0
+#define AT_RANGE_MAX 99
+#define AT_RANGE_DEFAULT 50
+
+#define AT_ASSIGN_MIN 0
+#define AT_ASSIGN_MAX 7
+#define AT_ASSIGN_DEFAULT 0 // Bitmapped: 0: Pitch, 1: Amp, 2: Bias
+
+#define AT_MODE_MIN 0
+#define AT_MODE_MAX MIDI_CONTROLLER_MODE_MAX
+#define AT_MODE_DEFAULT 0
 
 /*****************************************************
    CODE: orig_code/synth.h
