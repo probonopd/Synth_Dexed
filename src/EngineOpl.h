deb09052e97760eed277606951f469b48fe2c6f7
Holger Wirtz <wirtz@parasitstudio.de>
2023-03-21 09:10:45 +0100

    Fixing problems due to not declare render()-method as pure virtual in fm_core.h Small additional fixes.

--- src/EngineOpl.h
+++ src/EngineOpl.h
@@ -28,6 +28,8 @@
 
 class EngineOpl : public FmCore {
 public:
+    EngineOpl() {};
+    ~EngineOpl() {};
     void render(int32_t *output, FmOpParams *params, int32_t algorithm, int32_t *fb_buf, int32_t feedback_shift);
     void compute(int32_t *output, const int32_t *input, int32_t phase0, int32_t freq, int32_t gain1, int32_t gain2, bool add);
     void compute_pure(int32_t *output, int32_t phase0, int32_t freq, int32_t gain1, int32_t gain2, bool add);
--- src/fm_core.h
+++ src/fm_core.h
@@ -46,10 +46,11 @@ class FmAlgorithm {
 
 class FmCore {
   public:
-    //virtual ~FmCore() {};
+    FmCore() {};
+    virtual ~FmCore() {};
     static void dump();
-    uint8_t get_carrier_operators(uint8_t algorithm);
-    virtual void render(int32_t *output, FmOpParams *params, int32_t algorithm, int32_t *fb_buf, int32_t feedback_gain);
+    static uint8_t get_carrier_operators(uint8_t algorithm);
+    virtual void render(int32_t *output, FmOpParams *params, int32_t algorithm, int32_t *fb_buf, int32_t feedback_gain) = 0;
   protected:
     AlignedBuf<int32_t, _N_>buf_[2];
     const static FmAlgorithm algorithms[32];
