cb037ce324ca8eb907d1f77d5c5f7a2948daceff
Holger Wirtz <wirtz@parasitstudio.de>
2024-07-29 12:54:58 +0200

    Fixing example.

--- examples/SimpleDexed/SimpleDexed.ino
+++ examples/SimpleDexed/SimpleDexed.ino
@@ -1,35 +1,23 @@
 #define MIDI_CHANNEL 1
-#define MIDI_DEVICE_USB_HOST
 #define MIDI_DEVICE_DIN Serial1
+//#define  USB_AUDIO_INTERFACE
 
 #include <Audio.h>
-#include <MIDI.h>
 #include "synth_dexed.h"
-#if defined(MIDI_DEVICE_USB_HOST)
-#include <USBHost_t36.h>
-#endif
 
 AudioSynthDexed dexed(4, SAMPLE_RATE);  // 4 voices max
 AudioOutputI2S i2s1;
 AudioControlSGTL5000 sgtl5000_1;
 AudioConnection patchCord1(dexed, 0, i2s1, 0);
 AudioConnection patchCord2(dexed, 0, i2s1, 1);
-#ifdef AUDIO_INTERFACE
+#if defined(USB_AUDIO_INTERFACE)
 AudioOutputUSB usb1;
 AudioConnection patchCord3(dexed, 0, usb1, 0);
 AudioConnection patchCord4(dexed, 0, usb1, 1);
 #endif
 
-MIDI_CREATE_INSTANCE(HardwareSerial, MIDI_DEVICE_DIN, midi_serial);
-#if defined(MIDI_DEVICE_USB_HOST)
-USBHost usb_host;
-#endif
-#ifdef MIDI_DEVICE_USB_HOST
-MIDIDevice midi_usb(usb_host);
-#endif
-
 void setup() {
-  AudioMemory(32);
+  AudioMemory(16);
 
   sgtl5000_1.enable();
   sgtl5000_1.lineOutLevel(29);
@@ -39,47 +27,20 @@ void setup() {
   sgtl5000_1.unmuteLineout();
   sgtl5000_1.volume(0.8, 0.8);  // Headphone volume
 
-  midi_serial.begin(MIDI_CHANNEL_OMNI);
-#if defined(MIDI_DEVICE_USB_HOST)
-  usb_host.begin();
-#endif
-  dexed.setTranspose(36);
+  MIDI_DEVICE_DIN.begin(31250);
 }
 
 void loop() {
-  int8_t type;
-  int8_t midi_data[3];
-  int16_t SysExLength;
+  uint8_t midi_buffer[150];
+  uint8_t midi_len=0;
 
-  for (uint8_t i = 0; i < 2; i++) {
-    if (i == 0) {
-      midi_serial.read();
-
-      type = midi_serial.getType();
-      midi_data[0] = midi_serial.getChannel();
-      midi_data[1] = midi_serial.getData1();
-      midi_data[2] = midi_serial.getData2();
-
-      if (type != midi::SystemExclusive) {
-        dexed.midiDataHandler(midi_data[0], midi_data, 3);
-      } else {
-        SysExLength = midi_data[1] + midi_data[2] * 256;
-        dexed.midiDataHandler(midi_data[0], midi_serial.getSysExArray(), SysExLength);
-      }
-    } else {
-      midi_usb.read();
-
-      type = midi_usb.getType();
-      midi_data[0] = midi_usb.getChannel();
-      midi_data[1] = midi_usb.getData1();
-      midi_data[2] = midi_usb.getData2();
-
-      if (type != midi::SystemExclusive) {
-        dexed.midiDataHandler(midi_data[0], midi_data, 3);
-      } else {
-        SysExLength = midi_data[1] + midi_data[2] * 256;
-        dexed.midiDataHandler(midi_data[0], midi_usb.getSysExArray(), SysExLength);
-      }
-    }
+  while(MIDI_DEVICE_DIN.available()) {
+    if(midi_len<150)
+    	midi_buffer[midi_len++]=MIDI_DEVICE_DIN.read();
+    else
+        Serial.println("MIDI input buffer overflow!");
   }
+
+  if(midi_len > 0)
+      dexed.midiDataHandler(MIDI_CHANNEL, midi_buffer, midi_len);
 }
